32a0c0719f11970a4068b961b10d580ced951f40
Edouaria <e.lebourgeois@onlineformapro.com>
2022-04-26 16:49:55 +0100

    change font with react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import "@fontsource/aclonica"
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant='success' className="maclass">TOTO</Button>
+        <p className='maclassp'>Hello world !</p>
       </header>
     </div>
   );
